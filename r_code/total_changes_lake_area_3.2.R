e451f7ddf12836cc73a7d0ad1bdef77130936915
Ava Keerak <ava.keerak@ucalgary.ca>
2026-06-25 12:17:53 -0600

    added comments

--- r_code/total_changes_lake_area_3.2.R
+++ r_code/total_changes_lake_area_3.2.R
@@ -1,3 +1,8 @@
+# This is the R code used for section "3.2 Total Changes in Glacial Lake Area".
+# It uses the uncertainties calculated in uncertainties_2.4.R (from the dataframe named error_calc)
+# to create Fig. 7, a plot of the total lake area with error bars. Microsoft Excel code from B. Karchewski
+# (2025) was used to create Fig. 8, a plot of the rate of change in the total lake area.
+
 library(ggplot2)
 
 # uses error_calc from uncertaities file
@@ -25,7 +30,7 @@ ggplot(error_calc, aes(x = Year, y = AREA_GEO_km2)) +
   scale_linetype_manual(values = c("2nd Order Polynomial" = "dashed", "3rd Order Polynomial" = "dotted", "Error Bars" = "solid")) +
   theme_bw() + theme(text = element_text(size = 16, family = "Cambria"))
 
-# import file with rates of change in lake area (calculated in Microsoft Excel using code from B. Karchewski, 2025)
+# import file with rates of change in lake area (calculated in Microsoft Excel (version 2605) using code from B. Karchewski, 2025)
 total_areas_rchange <- your_data_here 
 
 # Figure 8 (plot of rates of change in total lake areas)
